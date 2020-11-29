86dca35372652102133b174842f942d9c7f6d75f
Oliver <oliverjamarie@gmail.com>
2020-11-29 14:23:00 +0100

    Connection.py Update
    Update to get user permissions

--- Connection.py
+++ Connection.py
@@ -1,6 +1,6 @@
 import mysql.connector as connector
 import json
-
+import re
 from mysql.connector import cursor
 
 class Connection:
@@ -24,6 +24,7 @@ class Connection:
             database = db_name
         )
 
+
         self.cursor = self.db.cursor()
 
         self.tables = []
@@ -36,7 +37,50 @@ class Connection:
             else:
                 self.tables.append(x[0])
 
+        self.privileges = self.getPrivileges()
+
+    # returns a list for the result
     def execute(self, queryMsg:str = ''):
         self.cursor.execute(queryMsg)
         
         return self.cursor.fetchall()
+
+    def getPrivileges(self):
+        cmd = 'SHOW GRANTS FOR \'{}\'@\'localhost\''.format(self.name)
+        result = self.execute(cmd)
+        
+        resultSTR = ''
+        for i in result: 
+            resultSTR += i[0]
+
+        print (resultSTR)
+        #print(type('ON `{}.*'.format(self.db.database)))
+
+        if ('ON `{}`.*'.format(self.db.database) in resultSTR):
+            print('Access to all tables')
+            return ['User has access to all tables']
+        
+        if (self.db.user == 'root'):
+            return ['User has root access']
+        
+        # Regular expression to extract the permissions 
+        # \`[a-z|A-Z]+\`\.\`[a-z|A-Z|_]+\`
+
+
+        parse = re.findall('\`[a-z|A-Z]+\`\.\`[a-z|A-Z|_]+\`', resultSTR)
+
+        perms = []
+
+        for i in parse:
+            li = i.split('.')
+            perms.append(li[1][1:-1]) #string slicing to remeove the quotation marks
+
+            
+
+        return perms
+
+
+        
+
+
+        
